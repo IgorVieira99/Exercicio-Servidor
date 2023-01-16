dc57304c20fa6809392ddf80c00595bb0a5ec188
Igor Vieira <114115230+IgorVieira99@users.noreply.github.com>
2023-01-16 20:07:46 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 ///JSON-SERVER
-import jsonServer from 'json-server';
+import jsonServer from  'C:/Users/programadores.carioc/node_modules/json-server'; //'C:.\Users\programadores.carioc\node_modules\json-server'
 const server = jsonServer.create();
 const router = jsonServer.router('database.json'); 
 const middlewares = jsonServer.defaults();
